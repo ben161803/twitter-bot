306d5ffddbf8df8561e48812a10e47ab3714c24d
Ben Levy <ben161803@gmail.com>
2018-07-25 21:23:13 -0500

    Base files

---
+++ ChatterBot.py
@@ -0,0 +1,19 @@
+# Dependencies
+import tweepy
+import json
+import time
+
+# Twitter API Keys
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# Setup Tweepy API Authentication
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+
+# Create a function that tweets
+# CODE GOES HERE
